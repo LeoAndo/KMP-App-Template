c4016d1e5444d0e0f2f1ef6de3f235c834f744fe
LeoAndo <leo.777.info@gmail.com>
2025-05-16 09:47:58 +0900

    Themeのパターンだし

---
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/theme/AppThemeType.kt
@@ -0,0 +1,18 @@
+package com.jetbrains.kmpapp.screens.theme
+
+import androidx.compose.material3.ColorScheme
+
+internal enum class AppThemeType(val lightColorScheme: ColorScheme, val darkColorScheme: ColorScheme) {
+    TYPE01(
+        com.jetbrains.kmpapp.screens.theme.type01.lightScheme,
+        com.jetbrains.kmpapp.screens.theme.type01.darkScheme
+    ),
+    TYPE02(
+        com.jetbrains.kmpapp.screens.theme.type02.lightScheme,
+        com.jetbrains.kmpapp.screens.theme.type02.darkScheme
+    ),
+    TYPE03(
+        com.jetbrains.kmpapp.screens.theme.type03.lightScheme,
+        com.jetbrains.kmpapp.screens.theme.type03.darkScheme
+    ),
+}
--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/theme/Theme.kt
@@ -2,21 +2,14 @@ package com.jetbrains.kmpapp.screens.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val lightColorScheme =
-    lightColorScheme(primary = md_theme_light_primary, secondary = md_theme_light_secondary)
-
-private val darkColorScheme =
-    darkColorScheme(primary = md_theme_dark_primary, secondary = md_theme_dark_secondary)
-
 @Composable
-fun MyMaterialTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+internal fun MyMaterialTheme(
+    appThemeType: AppThemeType = AppThemeType.TYPE01,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) darkColorScheme else lightColorScheme
+    val colors =
+        if (isSystemInDarkTheme()) appThemeType.darkColorScheme else appThemeType.lightColorScheme
     MaterialTheme(colorScheme = colors, content = content)
 }
