edbd56685dfd9e566ba3c9bb03d1dfd326117d85
LeoAndo <leo.777.info@gmail.com>
2025-05-18 22:55:45 +0900

    update

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/domain/model/PokemonModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/domain/model/PokemonModel.kt
@@ -1,5 +1,9 @@
 package com.jetbrains.kmpapp.domain.model
 
+/**
+ * coilを使ったgifアニメーションはAndroidのみサポートしている.
+ * https://coil-kt.github.io/coil/gifs/
+ */
 internal data class PokemonModel(
     val id: Int,
     val name: String,
