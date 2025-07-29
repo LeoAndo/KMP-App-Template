cca66e0c10bf51b3893933bb25f3c8f7fe6f2862
LeoAndo <leo.777.info@gmail.com>
2025-07-29 23:39:14 +0900

    update

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/quiz/QuizScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/quiz/QuizScreen.kt
@@ -1,63 +1,82 @@
 package com.jetbrains.kmpapp.screens.quiz
 
-import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 internal fun QuizScreen() {
+    val hostState = remember { SnackbarHostState() }
     val pagerState = rememberPagerState(pageCount = { Question.entries.size })
     val scope = rememberCoroutineScope()
     val scrollState = rememberScrollState()
     var isFinishedQuiz by remember { mutableStateOf(false) }
     var collectAnswerCount by remember { mutableStateOf(0) }
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(WindowInsets.safeDrawing.asPaddingValues()).padding(12.dp)
-            .verticalScroll(scrollState),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        HorizontalPager(
-            modifier = Modifier.heightIn(min = 400.dp)
-                .background(color = MaterialTheme.colorScheme.secondary),
-            state = pagerState,
-            userScrollEnabled = false,
-        ) { pageIndex ->
-            Box(
-                Modifier.padding(8.dp).fillMaxSize(), contentAlignment = Alignment.Center
-            ) {
-                val message =
-                    Question.entries.first { it.ordinal == pageIndex }.message
-                Text(text = message, overflow = TextOverflow.Ellipsis)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = hostState) }
+    ) { innerPadding ->
+        // メインコンテンツをここに記述する
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding).padding(12.dp)
+                .verticalScroll(scrollState),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                userScrollEnabled = false,
+            ) { pageIndex ->
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .aspectRatio(1f)
+                            .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                            .padding(12.dp), // 文字の周りに余白を追加する
+                        contentAlignment = Alignment.Center
+                    ) {
+                        val message = Question.entries.first { it.ordinal == pageIndex }.message
+                        Text(
+                            text = message,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    }
+                }
             }
-        }
 
-        AnimatedVisibility(isFinishedQuiz) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("${collectAnswerCount}問正解しました。")
+            if (isFinishedQuiz) {
+                Text("${collectAnswerCount}問正解しました")
                 Button(onClick = {
                     scope.launch {
                         pagerState.scrollToPage(page = 0)
@@ -68,25 +87,38 @@ internal fun QuizScreen() {
                     Text("やり直す")
                 }
             }
-        }
-        val question = Question.entries
-            .first { question -> question.ordinal == pagerState.currentPage }
-        question.answers.forEachIndexed { index, answerText ->
-            Button(
-                onClick = {
-                    scope.launch {
-                        isFinishedQuiz =
-                            Question.entries.size <= pagerState.currentPage + 1
-                        pagerState.scrollToPage(page = pagerState.currentPage + 1)
-                        if (question.answerIndex == index) { // 正解
-                            collectAnswerCount++
+
+            val question = Question.entries
+                .first { question -> question.ordinal == pagerState.currentPage }
+            question.answers.forEachIndexed { index, answerText ->
+                Button(
+                    enabled = !isFinishedQuiz,
+                    modifier = Modifier.widthIn(max = 320.dp).fillMaxWidth(0.5f),
+                    onClick = {
+                        scope.launch {
+                            isFinishedQuiz =
+                                Question.entries.size <= pagerState.currentPage + 1
+                            if (!isFinishedQuiz) {
+                                pagerState.scrollToPage(page = pagerState.currentPage + 1)
+                            }
+                            val message = if (question.answerIndex == index) {
+                                collectAnswerCount++
+                                "正解です！"
+                            } else {
+                                "不正解です！"
+                            }
+                            hostState.currentSnackbarData?.dismiss()
+                            hostState.showSnackbar(message)
                         }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth(),
-                enabled = !isFinishedQuiz,
-            ) {
-                Text(text = answerText, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                    },
+                ) {
+                    Text(
+                        text = answerText,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
     }
